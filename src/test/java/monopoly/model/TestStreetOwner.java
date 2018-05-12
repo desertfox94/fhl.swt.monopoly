46b74924ee62d559db42494dcb0645c538559ff4
unknown <florian.nickel@stud.fh-luebeck.de>
2018-05-12 11:44:15 +0200

    Test- CardOwener / StreetOwner added

---
+++ src/test/java/monopoly/model/TestStreetOwner.java
@@ -0,0 +1,16 @@
+package monopoly.model;
+
+
+import org.junit.Test;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Florian Nickel
+ */
+public class TestStreetOwner {
+
+}
