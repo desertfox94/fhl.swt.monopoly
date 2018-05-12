e622b66800c48c328c3bb8a3674f924f23dad570
Jake Stradling <jake.stradling@stud.fh-luebeck.de>
2018-05-12 09:01:40 +0200

    Added Unit Tests for Die

--- src/test/java/monopoly/model/TestDie.java
+++ src/test/java/monopoly/model/TestDie.java
@@ -3,8 +3,12 @@ package monopoly.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * @author Jake Stradling
+ */
 public class TestDie {
 
     @Test
@@ -17,6 +21,27 @@ public class TestDie {
             die2.roll();
         }
         assertTrue(die1.equals(die2));
+        assertFalse(die1.equals(new Integer(0)));
 
     }
+
+    @Test
+    public void testRoll(){
+        Die die = new Die();
+        for(int i = 0; i < 30; i++){
+            int roll = die.roll();
+            if(roll > 6 || roll < 1)
+                assertTrue(false);
+            return;
+        }
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGetNumber(){
+        Die die = new Die();
+        int roll = die.roll();
+        assertTrue(die.getNumber().get() == roll);
+    }
+
 }
