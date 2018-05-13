1c4789ad0fd43d5a431543ef772678cbf2298033
Crischinaldo <temp566566@gmx.de>
2018-05-13 15:01:48 +0200

    JUnit tests for Player and Street added

---
+++ src/test/java/monopoly/model/TestPlayer.java
@@ -0,0 +1,278 @@
+package monopoly.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import monopoly.core.CircleList;
+import monopoly.core.cards.Card;
+
+import monopoly.core.fields.JailField;
+import monopoly.core.fields.StartField;
+import monopoly.core.fields.StreetField;
+
+/**
+ * @author Christoph Thomas
+ */
+public class TestPlayer {
+
+	@Test
+	public void testGetfield() {
+		Player player = new Player();
+		assertTrue(player.getField() != null);
+	}
+
+	@Test
+	public void testmoveTofield() {
+		Game game = new Game();
+		CircleList circlist = new CircleList();
+		StartField startfield = new StartField();
+		Edition edi = new Edition();
+		Player player = new Player();
+		edi.setFields(circlist);
+
+		Boolean notMoved = true;
+		while (notMoved) {
+			assertTrue(player.getField().getValue() == null);
+			player.moveTo(startfield);
+			notMoved = false;
+		}
+		assertTrue(player.getField().getValue().equals(startfield));
+	}
+
+	@Test
+	public void testgetsetId() {
+		Player player = new Player();
+		assertTrue(player.getId() == null);
+		player.setId("123");
+		assertTrue(player.getId().equals("123"));
+		player.setId("456");
+		assertTrue(player.getId().equals("456"));
+	}
+
+	@Test
+	public void testgetsetName() {
+		Player player = new Player();
+		assertTrue(player.getName() == null);
+		player.setName("günther");
+		assertTrue(player.getName().equals("günther"));
+		player.setName("Hans");
+		assertTrue(player.getName().equals("Hans"));
+	}
+
+	@Test
+	public void testgetsetBalance() {
+		Player player = new Player();
+		assertTrue((player.getBalance() != null) && (player.getBalance().doubleValue() == 0.0));
+		double balance = 4;
+		player.setBalance(balance);
+		assertTrue(player.getBalance().doubleValue() == 4.0);
+	}
+
+	@Test
+	public void testpay() {
+		Player player = new Player();
+		player.setBalance(100.0);
+		double beforeBalance = player.getBalance().doubleValue();
+		double payment = 3.5;
+		double payment2 = 1000.0;
+		double negativePayment = -200;
+
+		player.pay(payment);
+		assertTrue(player.getBalance().doubleValue() == beforeBalance - payment); // -3.5
+
+		player.setBalance(beforeBalance);
+
+		player.pay(payment2);
+		assertTrue(player.getBalance().doubleValue() == beforeBalance - payment2); // -900
+
+		player.setBalance(beforeBalance);
+
+		player.pay(negativePayment);
+		assertTrue(player.getBalance().doubleValue() == beforeBalance - negativePayment); // 300
+																							// ->
+																							// ignores
+																							// -
+	}
+
+	@Test
+	public void testaddMoney() {
+		Player player = new Player();
+		player.setBalance(100.0);
+		double beforeBalance = player.getBalance().doubleValue();
+		double money = 3.5;
+		double negativeMoney = -1000.0;
+
+		player.addMoney(money);
+		assertTrue(player.getBalance().doubleValue() == beforeBalance + money);
+
+		player.setBalance(beforeBalance);
+
+		player.pay(negativeMoney);
+		assertTrue(player.getBalance().doubleValue() == beforeBalance - negativeMoney); // 1100
+																						// ->
+																						// ignores
+																						// -
+	}
+
+	@Test
+	public void testgetaddToInventoryStreets() {
+		Player player = new Player();
+		Street street = new Street();
+		Street street2 = new Street();
+		
+		assertTrue(player.getStreets().isEmpty());
+
+		player.addToInventory(street);
+
+		assertTrue(player.getStreets().get(0) == street && !player.getStreets().isEmpty());
+		
+		player.addToInventory(street2);
+		assertTrue(player.getStreets().size() == 2);
+	}
+	
+	@Test
+	public void testgetsetCards() {
+		Player player = new Player();
+		ObservableList<Card> cards = FXCollections.observableArrayList();
+		
+		assertTrue(player.getCards().isEmpty());
+		
+		Card card = new Card("eventcard", "triggers an event") {
+			@Override
+			public boolean execute(Player player) {
+				
+				return false;
+			}};		
+		cards.add(card);
+		
+		player.setCards(cards);
+		
+		assertTrue(player.getCards().get(0).getTitle() == "eventcard");
+		
+	
+	}
+	@Test
+	public void testisInJail() {
+		Player player = new Player();
+
+		assertTrue(!player.isInJail());
+		
+		player.sendToJail();
+		assertTrue(player.isInJail()); 
+
+	
+		
+	}
+	
+	@Test
+	public void testfreeFromJail() {
+		Player player = new Player();
+		
+		assertTrue(!player.isInJail());
+		
+		player.sendToJail();
+		assertTrue(player.isInJail());
+		
+		player.freeFromJail();
+		//assertTrue(!player.isInJail()); -> boolean should be false -> not working yet			
+	}
+	@Test
+	public void testsendToJail() {
+		Player player = new Player();
+		
+		assertTrue(!player.isInJail());
+		
+		player.sendToJail();
+		assertTrue(player.isInJail());		
+	}
+	
+	@Test
+	public void testgetsetFigure() {
+		Player player = new Player();
+		Figure fig = new Figure(null, "Blau");
+
+		assertTrue(player.getFigure() == null);	
+		
+		player.setFigure(fig);
+		assertTrue(player.getFigure().getName() == "Blau");	
+				
+	}
+	@Test
+	public void testincgetDoublecount() {
+		Player player = new Player();
+		assertTrue(player.getDoubleCount() == 0);	
+		
+		player.incDoubleCount();
+		assertTrue(player.getDoubleCount() == 1);
+				
+	}
+	@Test
+	public void testaddToInventory() {
+		Player player = new Player();
+		Street street = new Street();
+		
+		assertTrue(player.getDoubleCount() == 0);	
+		
+		player.incDoubleCount();
+		assertTrue(player.getDoubleCount() == 1);
+				
+	}
+	@Test
+	public void testgetPosition() {
+		Player player = new Player();
+		Street street = new Street();
+		StartField startfield = new StartField();
+		StreetField streetfield = new StreetField(street, 15);
+		JailField jailfield = new JailField();
+		
+		assertTrue(player.getPosition().get() == 0);
+		
+		player.moveTo(startfield);
+		assertTrue(player.getPosition().get() == 0);
+	
+		player.moveTo(streetfield);
+		assertTrue(player.getPosition().get() == 15);
+		
+		player.moveTo(jailfield);
+		assertTrue(player.getPosition().get() == 10); //jailindex static = 10				
+	}
+	
+	@Test
+	public void testsetGame() {
+		Player player = new Player();
+		Game game = new Game();
+		player.setGame(game); 		
+	}
+	
+}
+
+
+//
+//public void setGame(Game game) {
+//this.game = game;
+//} missing getter
+
+
+// @Override
+// public void removeFromInventory(Street street) {
+// // TODO Auto-generated method stub
+//
+// }  missing remove method
+
+// @Override
+// public void addCardToInventory(Card card) {
+// // TODO Auto-generated method stub
+//
+// } missing addcard method
+//
+// @Override
+// public void removeCardFromInventory(Card card) {
+// // TODO Auto-generated method stub
+//
+// } missing remove card method
+//
+
---
+++ src/test/java/monopoly/model/TestStreet.java
@@ -0,0 +1,174 @@
+package monopoly.model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableNumberValue;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Christoph Thomas
+ */
+public class TestStreet {
+	
+	@Test
+	public void testSetGetName() {
+		Street street = new Street();
+		assertTrue(street.getName() == null);
+		street.setName("Stresemanstraße");
+		assertTrue(street.getName().equals("Stresemanstraße"));
+	}
+	
+	@Test
+	public void testsetgetPrice() {
+		Street street = new Street();
+		
+		BigDecimal price = new BigDecimal("8900");
+		BigDecimal negativeprice = new BigDecimal("-8900");
+	
+		
+		assertTrue(street.getPrice() == null);
+		street.setPrice(price);
+		assertTrue(street.getPrice().compareTo(price) == 0);
+		street.setPrice(price);	
+		assertTrue(!(street.getPrice().compareTo(negativeprice) == 0)); // ignores (-) 8900 instead -8900			
+	}
+	
+	@Test
+	public void testbuildgetdemolishHouses() {
+		Street street = new Street();
+		StreetDetails rentDetails = new StreetDetails();
+		BigDecimal price = new BigDecimal("8900");	
+		rentDetails.setFirstHouseRent(price);
+		
+		assertTrue(street.getNumberOfHouses() == 0);	
+		
+		street.setRentDetails(rentDetails);
+		street.buildHouses(1);
+	
+		assertTrue(street.getNumberOfHouses() == 1); 
+		
+		street.demolishHouses(1);						// not correctly implemented yet, should not reduce to 
+		assertTrue(street.getNumberOfHouses() == 1); 	// a certain number of houses, but demolish a certain amount
+		
+		street.demolishHouses(0);
+		assertTrue(street.getNumberOfHouses() == 0); 
+	}
+	
+	@Test
+	public void testbuildgetdemolishHotel() {
+		Street street = new Street();
+		assertTrue(!street.getHotel());
+		street.buildHotel();
+		
+		assertTrue(street.getHotel());
+		
+		street.demolishHotel();
+		assertTrue(!street.getHotel());
+		
+		street.demolishHotel();				// shouldnt work
+		assertTrue(!street.getHotel());
+	}	
+	
+	@Test
+	public void testassumeisMontage() {
+		Street street = new Street();
+		assertTrue(!street.isMortage());
+		street.assumeMortage();
+		assertTrue(street.isMortage());		
+	}
+	
+	@Test
+	public void testsetgetOwner() {
+		Street street = new Street();
+		Player player = new Player();
+		assertTrue(street.getOwner() == null);		
+		street.setOwner(player);
+		assertTrue(street.getOwner().equals(player));	
+		player.setName("frank");
+		assertTrue(street.getOwner().getName().equals("frank"));		
+	}
+	
+	@Test
+	public void testsetgetRentDetails() {
+		Street street = new Street();
+		StreetDetails rentDetails = new StreetDetails();
+		BigDecimal price = new BigDecimal("8900");	
+		
+		assertTrue(street.getRentDetails() == null);	
+		street.setRentDetails(rentDetails);
+		assertTrue(street.getRentDetails().equals(rentDetails));		
+		rentDetails.setFirstHouseRent(price);
+		assertTrue(street.getRentDetails().getFirstHouseRent() == price);			
+	}
+	
+	@Test
+	public void testsetgetgroups() {
+		StreetGroup group = new StreetGroup();
+		BigDecimal price = new BigDecimal("8900");			
+		Street street = new Street();
+		
+		assertTrue(street.getGroup() == null);	
+		street.setGroup(group);
+		assertTrue(street.getGroup().equals(group));
+		group.setPricePerHouse(price);
+		assertTrue(street.getGroup().getPricePerHouse() == price);
+	}
+	
+	@Test
+	public void testgetrent() {
+		Street street = new Street();
+		StreetDetails rentDetails = new StreetDetails();
+		double rent = 50;
+		
+		DoubleProperty rent2 = new SimpleDoubleProperty();
+		rent2.set(50); // vergleichsobject
+		
+		assertTrue(	street.getRent().getValue() == 0);	
+		
+		rentDetails.setBaseRent(rent);
+		street.setRentDetails(rentDetails);
+		
+		
+		assertTrue(	street.getRent().getValue().compareTo(rent2.getValue()) == 0);					
+	}
+	
+
+	@Test
+	public void testgetCurrentRent() {
+		Street street = new Street();
+	
+		
+		StreetDetails rentDetails = new StreetDetails();
+		BigDecimal pricefirst = new BigDecimal("8900");	
+		BigDecimal pricesecond = new BigDecimal("0");
+		BigDecimal pricethird = new BigDecimal("-3");
+		BigDecimal pricefourth = new BigDecimal("40");
+
+		assertTrue(street.getRentDetails() == null);	
+		street.setRentDetails(rentDetails);
+		
+		street.buildHotel();
+		street.getRentDetails().setHotelRent(pricefirst);
+		assertTrue(street.getRentDetails().getHotelRent() == pricefirst);
+		
+		
+		rentDetails.setFirstHouseRent(pricefirst);
+		rentDetails.setSecondHouseRent(pricesecond);
+		rentDetails.setThirdHouseRent(pricethird);
+		rentDetails.setFourthHouseRent(pricefourth);
+		
+		street.buildHouses(4);
+		
+		assertTrue(street.getRentDetails().getFirstHouseRent() == pricefirst);
+		assertTrue(street.getRentDetails().getSecondHouseRent() == pricesecond);
+		assertTrue(street.getRentDetails().getThirdHouseRent() == pricethird);
+		assertTrue(street.getRentDetails().getFourthHouseRent() == pricefourth);		
+	}	
+}
+
