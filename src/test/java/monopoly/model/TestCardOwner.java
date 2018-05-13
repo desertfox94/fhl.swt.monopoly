1416f69b6c508fa7021d6803509baf9e45421229
unknown <florian.nickel@stud.fh-luebeck.de>
2018-05-13 11:28:11 +0200

    Tests für CardOwner und SteetOwner definiert

--- src/test/java/monopoly/model/TestCardOwner.java
+++ src/test/java/monopoly/model/TestCardOwner.java
@@ -12,5 +12,19 @@ import static org.junit.Assert.assertTrue;
  * @author Florian Nickel
  */
 public class TestCardOwner {
+    @Test
+    public void testAddCard(){
+        Payer player = new Payer();
+        Player.addCardToInventory(new CardFactory().getInstance().createEventCards()draw());
+        assertTrue(player.getCards().Count == 1);
+    }
 
+    @Test
+    public void testRemoveCard(){
+        Payer player = new Payer();
+        Card card = new CardFactory().getInstance().createEventCards()draw();
+        Player.addCardToInventory(card);
+        player.removeCardFromInventory(card);
+        assertTrue(player.getCards().Count == 0);
+    }
 }
