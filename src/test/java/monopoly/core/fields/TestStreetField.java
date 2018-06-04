0881dc1265a573930195689a85a63de2ffa5f61c
Jake Stradling <jake.stradling@stud.fh-luebeck.de>
2018-06-04 18:01:30 +0200

    M11: fixed test

--- src/test/java/monopoly/core/fields/TestStreetField.java
+++ src/test/java/monopoly/core/fields/TestStreetField.java
@@ -1,116 +1,106 @@
 package monopoly.core.fields;
 
-import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
-
-import java.math.BigDecimal;
-import java.security.MessageDigest;
-
-import monopoly.core.MessageUtil;
-import org.junit.Before;
-import org.junit.Test;
-
 import monopoly.model.Player;
 import monopoly.model.Street;
 import monopoly.model.StreetDetails;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Spy;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({MessageUtil.class})
+import java.math.BigDecimal;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+
 public class TestStreetField {
 
-    @Mock
-    private Street street;
 
-    @InjectMocks
+    @Mock
     StreetField streetField;
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
     }
 
-	@Test
-	public void testLandingAndAuction(){
-        PowerMockito.mockStatic(MessageUtil.class);
-        when(MessageUtil.ask("Strasse Kaufen", "Wollen Sie die Strasse Kaufen?", "ja", "nein, Auktion starten")).thenReturn(false);
-        Mockito.doThrow(new Exception("auction started")).when(street).startAuction();
+    @Test
+    public void testLandingAndAuction() {
         Player player = new Player();
+        Street street = new Street();
+         streetField = spy(new StreetField(street, 1));
+        //Mockito.when(streetField.ask()).thenReturn(false);
 
-        String message = "";
-        try {
-            streetField.landing(player);
-        }
-        catch (Exception e){
-        	message = e.getMessage();
-        }
-        if(message.equals("auction started")){
-        	assertTrue(true);
-        }
-        else{
-        	assertTrue(false);
-        }
+        doReturn(false).when(streetField).ask();
+        Whitebox.setInternalState(streetField, "street", street);
 
-	}
-
-	@Test
-    public void testLanding()
-    {
-		BigDecimal dec = new BigDecimal(500);
-	    Street street = new Street();
-	    Player player = new Player();
-	    Player playerTwo = new Player();
-	    Player playerThree = new Player();
-	    int index = 5;
-	    StreetField streetField = new StreetField(street, index);
-	    street.setName("ParkAllee");
-	    StreetDetails rentDetails = new StreetDetails();
-	    BigDecimal rentHouse = new BigDecimal(200);
-	    rentDetails.setFirstHouseRent(rentHouse);
-
-	    street.setRentDetails(rentDetails);
-	    street.buildHouses(1);
-	    street.setPrice(dec);
-
-	    player.addMoney(10000);
-
-	    streetField.buyStreet = true;
-	    streetField.landing(player);
-	    assertEquals(player.getBalance().doubleValue(), 9500.0);
-	    assertEquals(street.getOwner(), player);
-	    assertTrue(!player.getStreets().isEmpty());
-	    assertEquals(player.getStreets().get(0).getName(), "ParkAllee");
-	    streetField.buyStreet = false;
+        streetField.setStreet(street);
+        streetField.landing(player);
 
 
-	   playerTwo.addMoney(10000);
-
-	   assertEquals(player.getBalance().get(), 9500.0);
-	   streetField.landing(playerTwo);
-	   assertEquals(playerTwo.getBalance().get(), 9800.0);
-	   assertEquals(player.getBalance().get(), 9700.0);
-
-	   player.sendToJail();
-	   streetField.landing(playerTwo);
-	   assertEquals(playerTwo.getBalance().get(), 9800.0);
-	   assertEquals(player.getBalance().get(), 9700.0);
+        assertTrue(street.isAuctionObjective());
 
 
-	   playerThree.addMoney(100);
-	   assertEquals(playerThree.getBalance().doubleValue(), 100.0);
-	   playerThree.freeFromJail();
+    }
 
-	   assertEquals(player.getBalance().get(), 9700.0);
-	   streetField.landing(playerThree);
-	   assertEquals(playerThree.getBalance().get(), 100.0); // 100 statt -100 , betrag kann nicht negativ werden
-	   assertEquals(player.getBalance().get(), 9900.0);
+    @Test
+    public void testLanding() {
+        BigDecimal dec = new BigDecimal(500);
+        Street street = new Street();
+        Player player = new Player();
+        Player playerTwo = new Player();
+        Player playerThree = new Player();
+        int index = 5;
+        StreetField streetField = new StreetField(street, index);
+        street.setName("ParkAllee");
+        StreetDetails rentDetails = new StreetDetails();
+        BigDecimal rentHouse = new BigDecimal(200);
+        rentDetails.setFirstHouseRent(rentHouse);
+
+        street.setRentDetails(rentDetails);
+        street.buildHouses(1);
+        street.setPrice(dec);
+
+        player.addMoney(10000);
+
+        streetField.buyStreet = true;
+        streetField.landing(player);
+        assertEquals(player.getBalance().doubleValue(), 9500.0);
+        assertEquals(street.getOwner(), player);
+        assertTrue(!player.getStreets().isEmpty());
+        assertEquals(player.getStreets().get(0).getName(), "ParkAllee");
+        streetField.buyStreet = false;
+
+
+        playerTwo.addMoney(10000);
+
+        assertEquals(player.getBalance().get(), 9500.0);
+        streetField.landing(playerTwo);
+        assertEquals(playerTwo.getBalance().get(), 9800.0);
+        assertEquals(player.getBalance().get(), 9700.0);
+
+        player.sendToJail();
+        streetField.landing(playerTwo);
+        assertEquals(playerTwo.getBalance().get(), 9800.0);
+        assertEquals(player.getBalance().get(), 9700.0);
+
+
+        playerThree.addMoney(100);
+        assertEquals(playerThree.getBalance().doubleValue(), 100.0);
+        playerThree.freeFromJail();
+
+        assertEquals(player.getBalance().get(), 9700.0);
+        streetField.landing(playerThree);
+        assertEquals(playerThree.getBalance().get(), 100.0); // 100 statt -100 , betrag kann nicht negativ werden
+        assertEquals(player.getBalance().get(), 9900.0);
     }
 }
