3437b31eebf7e5ac6c162c0661fb2dbf1186ae5a
Crischinaldo <temp566566@gmx.de>
2018-06-06 21:27:12 +0200

    Deleted Tests on gui, saved local

--- src/test/java/monopoly/Gui/MonopolyGuiTest.java
+++ src/test/java/monopoly/Gui/MonopolyGuiTest.java
@@ -10,9 +10,6 @@ import javafx.scene.Parent;
 import javafx.stage.Stage;
 import monopoly.App;
 
-
-
-
 public class MonopolyGuiTest  {
 
 	public static class TestApp extends App  {
@@ -20,25 +17,20 @@ public class MonopolyGuiTest  {
 		
 		public TestApp () {
 			super();
-		}
-	
+		}	
 		@Override
 		public void start(Stage primaryStage) {
 			//primaryStage.initStyle(StageStyle.UNDECORATED);
 		    super.start(primaryStage);
 		}
-	
 		public static Stage getStage() {
 			return primaryStage;
-		}	
-		
+		}		
 	}
-
 	public static GuiTest controller;
 
 	  @Before
 	  public void setup() throws Exception {
-
 		    FXTestUtils.launchApp(TestApp.class);   
 		    controller = new GuiTest()
 		    {
@@ -50,28 +42,4 @@ public class MonopolyGuiTest  {
 		    };  	
 		  Thread.sleep(2000);
 	  }
-	  
-	@Test
-	public void testExists() {		
-		assertTrue(GuiTest.exists("Neues Spiel"));
-		assertTrue(GuiTest.exists("Spielstand laden"));	
-		assertTrue(GuiTest.exists("Spiel"));	
-	
-	}
-	
-	@Test
-	public void testClick() {	
-		
-		controller.click("Neues Spiel"); // doesnt work TODO
-		controller.click("Spielstand laden");  // doesnt work TODO
-		controller.click("Spiel");
-		controller.click("Neues Spiel!");
-		controller.click("Hilfe");
-		controller.click("Regelwerk");
-		controller.click("Spiel");
-		controller.click("Beenden");
-		assertTrue(GuiTest.exists("Cancel"));
-		controller.click("Cancel");
-	}
-
 }
